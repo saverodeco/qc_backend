e8f010dc179edda8dc3c0df52afa726d9020d21c
saverodeco <devoghassan@gmail.com>
2026-07-16 11:22:22 +0700

    ubah

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,11 @@
 use App\Http\Controllers\Api\QcController;
 use Illuminate\Support\Facades\Route;
 
-/**
- * Tempel isi file ini ke routes/api.php yang sudah ada,
- * atau require langsung dari sana.
- */
 Route::prefix('qc')->group(function () {
     Route::get('/', [QcController::class, 'index']);
     Route::get('lookup/{no_iml}', [QcController::class, 'lookup']);
+    Route::get('inspectors', [QcController::class, 'searchInspectors']);
     Route::post('{no_iml}/submit', [QcController::class, 'submit']);
     Route::post('{no_iml}/photos', [QcController::class, 'uploadPhoto']);
-    Route::patch('{no_iml}/photos/{id}', [QcController::class, 'updatePhotoMc']);
     Route::delete('{no_iml}/photos/{id}', [QcController::class, 'deletePhoto']);
-    Route::get('/qc/inspectors', [QcController::class, 'searchInspectors']);
-    Route::get('/qc/lookup/{no_iml}', [QcController::class, 'lookup']);
 });
